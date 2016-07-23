7aa5bc832e2b4e517c6905534cd920b662585f01
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2016-07-23 12:23:10 +0100

    Sigmoid correct

---
+++ Week3/machine-learning-ex2/ex2/test.m
@@ -0,0 +1,38 @@
+
+
+%% Initialization
+clear ; close all; clc
+
+%% Load Data
+%  The first two columns contains the exam scores and the third column
+%  contains the label.
+
+data = load('ex2data1.txt');
+X = data(:, [1, 2]); y = data(:, 3);
+
+data
+X
+
+
+
+%  Setup the data matrix appropriately, and add ones for the intercept term
+[m, n] = size(X);
+[m, n]
+% Add intercept term to x and X_test
+X = [ones(m, 1) X];
+X
+% Initialize fitting parameters
+initial_theta = zeros(n + 1, 1);
+initial_theta
+
+% Compute and display initial cost and gradient
+%[cost, grad] = costFunction(initial_theta, X, y);
+
+%fprintf('Cost at initial theta (zeros): %f\n', cost);
+%fprintf('Gradient at initial theta (zeros): \n');
+%fprintf(' %f \n', grad);
+
+%fprintf('\nProgram paused. Press enter to continue.\n');
+%pause;
+
+fprintf('sigmoid-------------------------------------');
