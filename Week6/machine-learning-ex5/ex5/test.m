31a50157f323e05dda0896e16ce460aeab0d0b25
BBK-PiJ-2015-07 <sftwracc.gmail.com>
2016-08-08 17:37:28 +0100

    modified test script

--- Week6/machine-learning-ex5/ex5/test.m
+++ Week6/machine-learning-ex5/ex5/test.m
@@ -79,4 +79,30 @@ hold off;
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
+%% =========== Part 5: Learning Curve for Linear Regression =============
+%  Next, you should implement the learningCurve function. 
+%
+%  Write Up Note: Since the model is underfitting the data, we expect to
+%                 see a graph with "high bias" -- slide 8 in ML-advice.pdf 
+%
 
+lambda = 0;
+[error_train, error_val] = ...
+    learningCurve([ones(m, 1) X], y, ...
+                  [ones(size(Xval, 1), 1) Xval], yval, ...
+                  lambda);
+
+plot(1:m, error_train, 1:m, error_val);
+title('Learning curve for linear regression')
+legend('Train', 'Cross Validation')
+xlabel('Number of training examples')
+ylabel('Error')
+axis([0 13 0 150])
+
+fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
+for i = 1:m
+    fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
+end
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
