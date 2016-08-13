51940797f1f98eb3462d26d75735bb146808e5d8
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2016-08-13 20:57:41 +0100

    Week8 - kMeansInitCentroid (random initialisation of centroids) done

--- Week8/machine-learning-ex7/ex7/kMeansInitCentroids.m
+++ Week8/machine-learning-ex7/ex7/kMeansInitCentroids.m
@@ -13,12 +13,8 @@ centroids = zeros(K, size(X, 2));
 %               the dataset X
 %
 
-
-
-
-
-
-
+rand_indices = randperm(size(X,1));
+centroids = X(rand_indices(1:K), :);
 
 % =============================================================
 
